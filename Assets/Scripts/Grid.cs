0d1ad8927ba6db9932426bfa07b02a34c67ed9bd
MaxColleoni <max.colleoni@gmail.com>
2024-07-22 20:51:58 -0400

    Creating tile programatically.

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,8 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
-    
+    public GameObject tilePrefab;
+    public Vector2 gridSize;
+    public int totalMines = 10;
+
+    List<Tile> tiles;
+
+    private void Start(){
+        Generate(gridSize);
+    }
+
+    public void Generate(Vector2 size){
+        tiles = new List<Tile>();
+
+        int count = 0;
+        for (int i = 0; i < size.x; i++){
+            for (int j = 0; j < size.y; j++){
+                Tile tile = GetTile(count, new Vector2(i, j));
+                tiles.Add(tile);
+                count++;
+            }
+        }
+        
+        Debug.LogError(tiles.Count);
+    }
+
+    Tile GetTile(int count, Vector2 position){
+        Tile tile = new Tile();
+        tile.index = count;
+        tile.position = position;
+        tile.isShown = false;
+        tile.isMine = GetIsMine();
+
+        return tile;
+    }
+
+    bool GetIsMine(){
+        
+        return false;
+    }
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tile : MonoBehaviour
+public class Tile
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public int index;
+    public int adjacentMines;
+    public bool isShown;
+    public bool isMine;
+    public Vector2 position;
+
+    void Show(){
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Flag(){
         
     }
 }
